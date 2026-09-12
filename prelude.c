93f5d2ece8c885f52a63835469a864c5e23099db
jhosein58 <jhosein58@gmail.com>
2026-09-12 23:18:32 +0330

    init

---
+++ prelude.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+void print_i32(int32_t v) {
+    printf("%d\n", v);
+}
+
+void print_star() {
+    printf("*");
+}
+
+void print_new_line() {
+    printf("\n");
+}
